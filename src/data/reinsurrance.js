1c4986c44d3d5bd46b84185ffec7cf334aae3d04
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-24 11:09:52 +0100

    🗃️  Add data for reinsurrance section

---
+++ src/data/reinsurrance.js
@@ -0,0 +1,27 @@
+import PictoChat from "../assets/pictograms/icon-chat.png";
+import PictoMoney from "../assets/pictograms/icon-money.png";
+import PictoShield from "../assets/pictograms/icon-security.png";
+
+const reinssuranceData = [
+	{
+		icon: PictoChat,
+		iconDescription: "Chat pictogram",
+		title: "You are our #1 priority",
+		description:
+			"Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5minutes.",
+	},
+	{
+		icon: PictoMoney,
+		iconDescription: "Money pictogram",
+		title: "More savings means higher rates",
+		description: "The more you save with us, the higher yout interest rate will be!",
+	},
+	{
+		icon: PictoShield,
+		iconDescription: "Shield pictogram",
+		title: "Security you can trust",
+		description: "We use top of the line encryption to make sure your data and money is always safe.",
+	},
+];
+
+export default reinssuranceData;
