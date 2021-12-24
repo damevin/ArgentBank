240c7fdb3d1146b7b80d424bd2d7be83deec6497
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-24 11:58:05 +0100

    🤡  Create mocked data for transactions

---
+++ src/data/transactions.js
@@ -0,0 +1,19 @@
+const transactionsData = [
+	{
+		type: "Checking (x8349)",
+		amount: 2082.79,
+		balance: "Available",
+	},
+	{
+		type: "Savings (x6712)",
+		amount: 10928.42,
+		balance: "Balance",
+	},
+	{
+		type: "Credit Card (x8349)",
+		amount: 184.3,
+		balance: "Balance",
+	},
+];
+
+export default transactionsData;
