5ef500ddde2576a9a83bdb0c9d5db2b7c90818ed
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-24 15:25:59 +0100

    🚧  WIP: Store, user, consumers

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import "./Navbar.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { setEmail, setToken } from "../../redux/reducers";
 import ButtonSecondary from "../Buttons/ButtonSecondary/ButtonSecondary";
 import Logo from "./logo.png";
 import React from "react";
 
 export default function Navbar() {
+	const token = useSelector((state) => state.user.token);
+	const dispatch = useDispatch();
+
 	return (
 		<nav className="navbar">
+			{token}
 			<img src={Logo} alt="" className="navbar__logo"></img>
 			<ButtonSecondary />
 		</nav>
