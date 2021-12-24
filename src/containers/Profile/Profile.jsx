a96a2e64280cdc449d835321428d3e9eebb8b971
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-24 11:58:16 +0100

    🚧 WIP: User profile

---
+++ src/containers/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Profile() {
+	return (
+		<div className="profile">
+			<header>
+				<h1>Welcome back</h1>
+				<h1>User name!</h1>
+				<button>Edit name</button>
+			</header>
+   <section>
+    
+   </section>
+		</div>
+	);
+}
