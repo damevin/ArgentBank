47bc8aee80ca2fd05793ac564120b3f96a72a52e
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-24 15:25:39 +0100

    🔨  Add provider for store

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Router from "./router";
 import Footer from "./components/Footer/Footer.jsx";
 import React from "react";
 
+import { Provider } from "react-redux";
+import { store } from "../src/redux/store";
+
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
-			<Router />
-			<Footer />
-		</div>
+		<Provider store={store}>
+			<div className="App">
+				<Navbar />
+				<Router />
+				<Footer />
+			</div>
+		</Provider>
 	);
 }
 
