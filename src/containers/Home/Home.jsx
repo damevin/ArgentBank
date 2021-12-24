0c41dc11922b5a4554912e6dd995f7d00e910aee
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-24 11:22:24 +0100

    🎨  Improve structure of homepage

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import reinssuranceData from "../../data/reinsurrance.js";
 import CardReinsurance from "../../components/Cards/CardReinsurance/CardReinsurance";
+import "./Home.scss";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="home">
 			<Header />
-			<section>
+			<section className="home__section">
 				{reinssuranceData.map((card) => {
 					return <CardReinsurance reinssurance={card} />;
 				})}
