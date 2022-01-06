4110e694e73fb55e521d463ddce7cd3364790554
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 14:05:19 +0100

    🤡  Update mocked data with _id value

--- src/data/reinsurrance.js
+++ src/data/reinsurrance.js
@@ -4,6 +4,7 @@ import PictoShield from "../assets/pictograms/icon-security.png";
 
 const reinssuranceData = [
 	{
+		_id: 1,
 		icon: PictoChat,
 		iconDescription: "Chat pictogram",
 		title: "You are our #1 priority",
@@ -11,12 +12,14 @@ const reinssuranceData = [
 			"Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5minutes.",
 	},
 	{
+		_id: 2,
 		icon: PictoMoney,
 		iconDescription: "Money pictogram",
 		title: "More savings means higher rates",
 		description: "The more you save with us, the higher yout interest rate will be!",
 	},
 	{
+		_id: 3,
 		icon: PictoShield,
 		iconDescription: "Shield pictogram",
 		title: "Security you can trust",
--- src/data/transactions.js
+++ src/data/transactions.js
@@ -1,15 +1,18 @@
 const transactionsData = [
 	{
+		_id: 1,
 		type: "Checking (x8349)",
 		amount: 2082.79,
 		balance: "Available",
 	},
 	{
+		_id: 2,
 		type: "Savings (x6712)",
 		amount: 10928.42,
 		balance: "Balance",
 	},
 	{
+		_id: 3,
 		type: "Credit Card (x8349)",
 		amount: 184.3,
 		balance: "Balance",
