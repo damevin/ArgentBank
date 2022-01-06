3d7ab4b3c76bdad5e3042dafbf7e17ea443d83af
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 13:12:28 +0100

    🎨  Create actions

---
+++ src/redux/actions.js
@@ -0,0 +1,60 @@
+export const TOKEN_SUCCESS = "TOKEN_SUCCESS";
+export const TOKEN_ERROR = "TOKEN_ERROR";
+export const USER_SUCCESS = "USER_SUCCESS";
+export const USER_ERROR = "USER_ERROR";
+export const USER_LOGOUT = "USER_LOGOUT";
+export const USER_EDIT_SUCCESS = "USER_EDIT_SUCESS";
+export const USER_EDIT_ERROR = "USER_EDIT_ERROR";
+
+export const fetchTokenSuccess = (token) => {
+	return {
+		type: TOKEN_SUCCESS,
+		token,
+		remember: token.body.remember,
+	};
+};
+
+export const fetchTokenError = (error) => ({
+	type: TOKEN_ERROR,
+	error,
+});
+
+export const fetchUserSuccess = (user) => {
+	return {
+		type: USER_SUCCESS,
+		user: user.body,
+		userLoading: false,
+		token: user.token,
+	};
+};
+
+export const fetchUserError = (userError) => ({
+	type: USER_ERROR,
+	userError,
+	userLoading: false,
+	token: false,
+});
+
+export const userLogout = () => {
+	return {
+		type: USER_LOGOUT,
+		user: {},
+		token: false,
+	};
+};
+
+export const userEditSuccess = (user) => {
+	return {
+		type: USER_EDIT_SUCCESS,
+		user: user.body,
+		userLoading: false,
+	};
+};
+
+export const userEditError = (userError) => {
+	return {
+		type: USER_EDIT_ERROR,
+		userError,
+		userLoading: false,
+	};
+};
