860071ab40702967b81d3b9d1140f20dadc86fef
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 14:06:41 +0100

    ♻️  Change where store provider is defined

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
 	<BrowserRouter>
 		<React.StrictMode>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</React.StrictMode>
 	</BrowserRouter>,
 	document.getElementById("root")
