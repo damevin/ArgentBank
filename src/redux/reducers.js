4f0fc2d8415ba14547639aaea771ac205d187738
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 13:12:13 +0100

    🎨  Redesign reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,29 +1,77 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { initialStateAuthentification, initialStateUser } from "./initialsStates";
+import {
+	TOKEN_ERROR,
+	TOKEN_SUCCESS,
+	USER_EDIT_ERROR,
+	USER_EDIT_SUCCESS,
+	USER_ERROR,
+	USER_LOGOUT,
+	USER_SUCCESS,
+} from "./actions";
 
-export const userSlice = createSlice({
-	name: "user",
-	initialState: {
-		email: "",
-		token: "",
-		firstname: "",
-		lastname: "",
-	},
-	reducers: {
-		setEmail: (state, action) => {
-			state.email = action.payload;
-		},
-		setToken: (state, action) => {
-			state.token = action.payload;
-		},
-		setFirstname: (state, action) => {
-			state.firstname = action.payload.firstName;
-		},
-		setLastname: (state, action) => {
-			state.lastname = action.payload.lastName;
-		},
-	},
-});
+const initialState = { ...initialStateAuthentification, ...initialStateUser };
 
-export const { setEmail, setToken, setFirstname, setLastname } = userSlice.actions;
+export const userAuthentification = (state = initialState, action) => {
+	switch (action.type) {
+		case TOKEN_SUCCESS: {
+			return {
+				...state,
+				token: action.token.body.token,
+				remember: action.token.body.remember,
+			};
+		}
+		case TOKEN_ERROR: {
+			return {
+				...state,
+				error: action.error,
+				loading: action.loading,
+			};
+		}
 
-export const userReducer = userSlice.reducer;
+		case USER_SUCCESS: {
+			return {
+				...state,
+				user: action.user,
+				userLoading: action.userLoading,
+				token: action.token,
+			};
+		}
+		case USER_ERROR: {
+			return {
+				...state,
+				userError: action.userError,
+				userLoading: action.userLoading,
+				token: action.token,
+			};
+		}
+
+		case USER_LOGOUT: {
+			if (localStorage.getItem("token")) {
+				localStorage.removeItem("token");
+			}
+			return {
+				...state,
+				user: {},
+				token: "",
+			};
+		}
+
+		case USER_EDIT_SUCCESS: {
+			return {
+				...state,
+				user: action.user,
+				userLoading: action.userLoading,
+			};
+		}
+
+		case USER_EDIT_ERROR: {
+			return {
+				...state,
+				userError: action.userError,
+				userLoading: action.userLoading,
+			};
+		}
+		default:
+			return state;
+	}
+};
