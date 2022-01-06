859ffcbd3f5a98190b0beff134e394622dd8aa49
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 16:39:14 +0100

    🚧  WIP: Buttons and more

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,44 @@
 import "./Navbar.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { setEmail, setToken } from "../../redux/reducers";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { USER_LOGOUT } from "../../redux/actions";
 import ButtonSecondary from "../Buttons/ButtonSecondary/ButtonSecondary";
 import Logo from "./logo.png";
 import React from "react";
 
-export default function Navbar() {
-	const token = useSelector((state) => state.user.token);
-	const dispatch = useDispatch();
-
-
+const Navbar = ({ token, user, userLogout }) => {
 	return (
 		<nav className="navbar">
-			<img src={Logo} alt="" className="navbar__logo"></img>
-			<ButtonSecondary />
+			<Link to="/">
+				<img src={Logo} alt="" className="navbar__logo"></img>
+			</Link>
+			{token ? (
+				<>
+					<Link to="/profile">
+						<button>{user.firstName}</button>
+					</Link>
+					<button onClick={() => userLogout()}>Logout</button>
+				</>
+			) : (
+				<Link to="/login">
+					<ButtonSecondary type="login" />
+				</Link>
+			)}
 		</nav>
 	);
-}
+};
+
+const mapStateToProps = ({ token, user }) => {
+	return {
+		token,
+		user,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		userLogout: () => dispatch({ type: USER_LOGOUT }),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
