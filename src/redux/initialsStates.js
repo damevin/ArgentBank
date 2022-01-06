c154339e6d82d647fb8a8f34f4c1ce893b75647f
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 13:11:07 +0100

    🎨  Create initials states

---
+++ src/redux/initialsStates.js
@@ -0,0 +1,13 @@
+export const initialStateAuthentification = {
+	token: "",
+	error: "",
+	remember: false,
+};
+
+export const initialStateUser = {
+	user: {
+		email: "",
+		firstName: "",
+		lastName: "",
+	},
+};
