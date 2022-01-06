2d66e1e4c074bdfc8f8178a232fe771acd25a9be
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 14:05:48 +0100

    🔨  User middleware

---
+++ src/redux/middlewares/user.js
@@ -0,0 +1,28 @@
+import { fetchUserSuccess, fetchUserError } from "../actions";
+
+const api = process.env.REACT_APP_API_URL;
+const url = `${api}/user`;
+
+export const fetchUser = (query) => {
+	return async (dispatch) => {
+		return fetch(`${url}/${query.endPoint}`, {
+			method: query.method,
+			headers: {
+				Authorization: `Bearer ${query.token}`,
+			},
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((user) => {
+    console.log(user)
+				if (query.token) {
+					user.token = query.token;
+				}
+				dispatch(fetchUserSuccess(user));
+			})
+			.catch((userError) => {
+				dispatch(fetchUserError(true));
+			});
+	};
+};
