97afee66787be0e982ad7d059a5370312c482353
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-06 11:16:19 +0100

    🚧 WIP: Form login

--- src/components/Forms/FormLogin/FormLogin.jsx
+++ src/components/Forms/FormLogin/FormLogin.jsx
@@ -1,23 +1,84 @@
-import React from "react";
 import "./FormLogin.scss";
+import { Navigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { setEmail, setToken, setFirstname, setLastname } from "../../../redux/reducers";
+import React from "react";
 
 export default function FormLogin() {
+	const token = useSelector((state) => state.user.token);
+	const dispatch = useDispatch();
+
+	const [userInputs, setUserInputs] = useState({
+		email: "",
+		password: "",
+	});
+	const [formIsSubmited, setFormIsSubmited] = useState(false);
+	const [submitError, setSubmitError] = useState(false);
+
+	const inputChange = (event) => {
+		const inputType = event.target.id;
+		setUserInputs({
+			...userInputs,
+			[inputType]: event.target.value,
+		});
+		setSubmitError(false);
+	};
+
+	const loginFormSubmit = (event) => {
+		event.preventDefault();
+		setFormIsSubmited(true);
+	};
+
+	useEffect(() => {
+		if (formIsSubmited) {
+			const api = process.env.REACT_APP_API_URL;
+			const url = `${api}/user/login`;
+			const loginPayload = {
+				email: userInputs.email,
+				password: userInputs.password,
+			};
+
+			fetch(url, {
+				method: "POST",
+				body: JSON.stringify(loginPayload),
+				headers: { "Content-type": "application/json" },
+			})
+				.then((response) => response.json())
+				.then((json) => {
+					dispatch(setEmail(userInputs.email));
+					dispatch(setToken(json.body.token));
+
+				})
+				.catch((error) => {
+					setSubmitError(true);
+
+					setUserInputs({
+						...userInputs,
+					});
+				});
+			setFormIsSubmited(false);
+		}
+	}, [dispatch, userInputs, formIsSubmited]);
+
+	if (token) return <Navigate to="/profile" />;
+
 	return (
-		<form className="formLogin">
+		<form className="formLogin" onSubmit={loginFormSubmit}>
 			<div className="formLogin__wrapper">
 				<label className="formLogin__label" htmlFor="">
-					Username
+					E-mail
 				</label>
-				<input className="formLogin__input" type="text" />
+				<input className="formLogin__input" id="email" onChange={inputChange} type="text" />
 			</div>
 			<div className="formLogin__wrapper">
 				<label className="formLogin__label" htmlFor="">
 					Password
 				</label>
-				<input className="formLogin__input" type="password" />
+				<input className="formLogin__input" id="password" onChange={inputChange} type="password" />
 			</div>
 			<div className="formLogin__wrapper--secondary">
-				<input className="formLogin__input--secondary" type="checkbox" />
+				<input className="formLogin__input--secondary" id="save" type="checkbox" />
 				<label className="formLogin__label--secondary" htmlFor="">
 					Remember me
 				</label>
